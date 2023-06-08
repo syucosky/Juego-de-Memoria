233341dc5e3a632ca2e8a641f29f6b7d245401dd
syucosky <syucosky@gmail.com>
2023-06-07 22:43:06 -0300

    principio entrega 2

---
+++ app/src/main/java/com/example/juegodememoria/MenuActivity.kt
@@ -0,0 +1,21 @@
+package com.example.juegodememoria
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main);
+    }
+
+    fun comenzarJuegoBtn(v : View){
+        val i = Intent(this, GameActivity::class.java);
+        startActivity(i);
+    }
+    fun menuAyuda(v : View){
+        val i = Intent(this,)
+    }
+}
