c1e4187c6adca146830531cf393973299aeabd8b
syucosky <syucosky@gmail.com>
2023-06-08 22:43:52 -0300

    cambios en la img al girar el telefono en la pantalla principal, en land botones ajustados, en activity como jugar espacio entre texto acomodado para portait y land

--- app/src/main/java/com/example/juegodememoria/HelpActivity.kt
+++ app/src/main/java/com/example/juegodememoria/HelpActivity.kt
@@ -4,19 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
-class MenuActivity : AppCompatActivity() {
+class HelpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_help)
     }
-
     fun comenzarJuegoBtn(v : View){
         val i = Intent(this, GameActivity::class.java);
         startActivity(i);
     }
-    fun menuAyuda(v : View){
-        val i = Intent(this, HelpActivity::class.java);
+    fun volver(v : View){
+        val i = Intent(this, MenuActivity::class.java);
         startActivity(i);
+        super.onBackPressed();
+        finish();
     }
 }
